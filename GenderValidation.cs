53ce9012857265798de144989934e5c0cf5a14ab
miglena-dodleva <118475323+miglena-dodleva@users.noreply.github.com>
2023-03-24 06:10:13 +0200

    Add files via upload

---
+++ GenderValidation.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagementSystemApi
+{
+
+    public enum GenderType
+    {
+        Male,
+        Female
+    }
+    public class GenderValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var gender = value as GenderType?;
+            if (gender == null)
+            {
+                return new ValidationResult("Gender must be specified.");
+            }
+            else if (gender != GenderType.Male && gender != GenderType.Female)
+            {
+                return new ValidationResult("Invalid gender specified.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
